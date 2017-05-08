1e6e110dedcbd899424e0b3fa7740d5ffabb4e05
Samuel DeSota <me@samdesota.com>
2017-05-08 13:44:45 +0000

    added lang

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,8 +1,19 @@
 
+export LANG='en_US.UTF-8'
+
 antigen use oh-my-zsh
 antigen theme bira
 
 antigen bundle git
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle zsh-users/zsh-autosuggestions
+
+eval $(opam config env)
+
+export FZF_DEFAULT_COMMAND='
+  (git ls-tree -r --name-only HEAD ||
+   find . -path "*/\.*" -prune -o -type f -print -o -type l -print |
+      sed s/^..//) 2> /dev/null'
+
+bindkey -v
+export KEYTIMEOUT=1
+
 
